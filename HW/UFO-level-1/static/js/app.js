2a8aab6960c1325d67b6566cea774a74fa3a64f1
abigailrgreene <abigailrgreene@gmail.com>
2020-10-16 17:20:26 -0400

    part 1 done

--- HW/UFO-level-1/static/js/app.js
+++ HW/UFO-level-1/static/js/app.js
@@ -44,19 +44,19 @@ function runEnter() {
 
   console.log(filteredData);
 
-  var tbody = d3.select("tbody");
+  var ftable = d3.select("tbody");
+  ftable.html("")
 
-  data.forEach(function(filteredData) {
-    console.log(filteredData);
+
+  filteredData.forEach(function(result) {
+    console.log(result);
     var row = tbody.append("tr");
-    Object.entries(filteredData).forEach(function([key, value]) {
-      console.log(key, value);
-      //Append a cell to the row for each value
-      // in the weather report object
-      var cell = row.append("td");
-      cell.text(value);
-    });
+    Object.entries(result).forEach(function([key, value]) {
+        console.log(key, value);
+            // Append a cell to the row for each value
+        var cell = row.append("td");
+        cell.text(value);
+        });
+
   });
 };
-
- 
