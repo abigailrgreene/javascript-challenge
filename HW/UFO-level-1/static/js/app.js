38c2f19e0fbfa1ca71819ca2cd0eb3c071bf7e5b
abigailrgreene <abigailrgreene@gmail.com>
2020-10-16 14:06:07 -0400

    table append

--- HW/UFO-level-1/static/js/app.js
+++ HW/UFO-level-1/static/js/app.js
@@ -1,4 +1,16 @@
 // from data.js
-var tableData = data;
+var sighting = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+  data.forEach(function(sighting) {
+    console.log(sighting);
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
